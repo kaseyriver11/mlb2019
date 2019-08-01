f835616eeede8422d65a9c7ae0927246f188c807
Kasey Jones <krjones@rti.org>
2019-08-01 15:04:57 -0400

    Fixing address

--- docker/jupyter_notebook_config.py
+++ docker/jupyter_notebook_config.py
@@ -1,7 +1,7 @@
 c = get_config()  # get the config object
 c.IPKernelApp.pylab = 'inline'  # in-line figure when using Matplotlib
-c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.ip = '127.0.0.1'
 c.NotebookApp.open_browser = False  # do not open a browser window by default when using notebooks
 c.NotebookApp.token = '' # No token. Always use jupyter over ssh tunnel
 c.NotebookApp.notebook_dir = ''
-c.NotebookApp.custom_display_url = 'http://67.205.165.188:1111/' 
+c.NotebookApp.custom_display_url = 'http://localhost:1111' 
